c1f0b28cb91d6e122d73738409e21542f4ff143c
Mike C <mike.choi@live.ca>
2018-09-10 07:36:51 +0000

    Added sign-up new and create routes, and added auth using passport-local

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -21,7 +21,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 // Takes data, encodes it, and puts back into session
 passport.serializeUser(User.serializeUser());
 // Reads session, takes data and unencodes it
@@ -31,6 +30,7 @@ passport.deserializeUser(User.deserializeUser());
 // ==================== //
 //        ROUTES        //
 // ==================== //
+
 app.get("/", function(req, res){
     res.render("home");
 });
@@ -38,6 +38,26 @@ app.get("/", function(req, res){
 app.get("/secret", function(req, res){
     res.render("secret");
 });
+
+// Show sign-up form
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+// Handling user sign up
+app.post("/register", function(req, res){
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            res.render("register");
+        } 
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/secret");
+        });
+    });
+});
+
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server has started...");
