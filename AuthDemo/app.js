32ef7a1e1abd23b5202cad5f274486d18319208c
Mike C <mike.choi@live.ca>
2018-09-10 07:00:12 +0000

    Required packages and made user model with passport local mongoose plugin

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -1,16 +1,22 @@
-var express = require("express");
-var mongoose = require("mongoose");
-var bodyParser = require("body-parser");
-var app = express();
+var passportLocalMongoose   = require("passport-local-mongoose"),
+    LocalStrategy           = require("passport-local"),
+    User                    = require("./models/user"),
+    bodyParser              = require("body-parser"),
+    passport                = require("passport"),
+    mongoose                = require("mongoose"),
+    express                 = require("express"),
+    app                     = express();
 
 mongoose.connect("mongodb://localhost/auth_demo_app", {useNewUrlParser: true});
-app.use(bodyParser.urlncoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+// ====== //
+// ROUTES //
+// ====== //
 app.get("/", function(req, res){
     res.render("home");
 });
-
 
 app.get("/secret", function(req, res){
     res.render("secret");
