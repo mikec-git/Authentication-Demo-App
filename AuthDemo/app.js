7bb00f782a711b781fdaf95595b5d0950091eb6b
Mike C <mike.choi@live.ca>
2018-09-10 07:08:50 +0000

    Added passport dependencies for serialize/deserialize and passport middleware

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -4,16 +4,33 @@ var passportLocalMongoose   = require("passport-local-mongoose"),
     bodyParser              = require("body-parser"),
     passport                = require("passport"),
     mongoose                = require("mongoose"),
-    express                 = require("express"),
-    app                     = express();
+    express                 = require("express");
+    
+var app                     = express();
 
 mongoose.connect("mongodb://localhost/auth_demo_app", {useNewUrlParser: true});
-app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
-// ====== //
-// ROUTES //
-// ====== //
+app.use(require("express-session")({
+    secret: "Mustangs are better than Camaros",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// Takes data, encodes it, and puts back into session
+passport.serializeUser(User.serializeUser());
+// Reads session, takes data and unencodes it
+passport.deserializeUser(User.deserializeUser());
+
+
+// ==================== //
+//        ROUTES        //
+// ==================== //
 app.get("/", function(req, res){
     res.render("home");
 });
